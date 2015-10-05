4ed8fd1d162dcc2c2893881a0fe8f6e7247d8a76
Dante <deathzeroking@hotmail.com>
2015-10-05 14:15:03 +0800

    Initial Commit

---
+++ 05/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  05
+//
+//  Created by 鄭涵嚴 on 2015/10/5.
+//  Copyright © 2015年 鄭涵嚴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 05/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  05
+//
+//  Created by 鄭涵嚴 on 2015/10/5.
+//  Copyright © 2015年 鄭涵嚴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
